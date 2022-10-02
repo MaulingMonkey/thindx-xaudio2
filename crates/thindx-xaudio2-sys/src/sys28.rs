28cf73f3f55468fdb2cf1152f2bb94fa3ec8a6d8
MaulingMonkey <git@maulingmonkey.com>
2022-10-02 09:41:27 -0700

    XAudio 2.8 ..= 2.9: DLL and IID constants.

--- crates/thindx-xaudio2-sys/src/sys28.rs
+++ crates/thindx-xaudio2-sys/src/sys28.rs
@@ -5,3 +5,21 @@
 //! ### References
 //! *   [XAudio2 Versions: XAudio 2.8 (Windows 8.x)](https://learn.microsoft.com/en-us/windows/win32/xaudio2/xaudio2-versions#xaudio-28-windows-8x)
 //! *   [XAudio2 and Windows 8](https://walbourn.github.io/xaudio2-and-windows-8/)
+
+// don't use prev
+use abistr::*;
+use winapi::shared::guiddef::GUID;
+
+#[doc = "`\"xaudio2_8.dll\"`"] pub const XAUDIO2_DLL    : &'static str              =           "xaudio2_8.dll";
+#[doc = "`\"xaudio2_8.dll\"`"] pub const XAUDIO2_DLL_A  : CStrNonNull<'static, u8 > = cstr8!(   "xaudio2_8.dll");
+#[doc = "`\"xaudio2_8.dll\"`"] pub const XAUDIO2_DLL_W  : CStrNonNull<'static, u16> = cstr16!(  "xaudio2_8.dll");
+
+// There is no debug variant of xaudio2_8.dll.
+// As such, these constants weren't initially defined for xaudio2.h.
+// However, these are convenient if targeting 2.8 and 2.9 in the same build.
+// As such, define them, but hide them from the documentation.
+#[doc(hidden)] pub const XAUDIO2D_DLL   : &'static str              =           "xaudio2_8.dll";
+#[doc(hidden)] pub const XAUDIO2D_DLL_A : CStrNonNull<'static, u8 > = cstr8!(   "xaudio2_8.dll");
+#[doc(hidden)] pub const XAUDIO2D_DLL_W : CStrNonNull<'static, u16> = cstr16!(  "xaudio2_8.dll");
+
+#[doc = "`60d8dac8-5aa1-4e8e-b597-2f5e2883d484`"] pub const IID_IXAudio2 : GUID = super::guid(0x60d8dac8, 0x5aa1, 0x4e8e, 0xb5972f5e2883d484);
--- crates/thindx-xaudio2-sys/src/sys29.rs
+++ crates/thindx-xaudio2-sys/src/sys29.rs
@@ -7,3 +7,16 @@
 //! *   [Developer guide for redistributable version of XAudio 2.9](https://learn.microsoft.com/en-us/windows/win32/xaudio2/xaudio2-redistributable)
 
 //use super::xaudio2_8 as prev;
+use abistr::*;
+use winapi::shared::guiddef::GUID;
+
+#[doc = "`\"xaudio2_9.dll\"`"] pub const XAUDIO2_DLL        : &'static str              =           "xaudio2_9.dll";
+#[doc = "`\"xaudio2_9.dll\"`"] pub const XAUDIO2_DLL_A      : CStrNonNull<'static, u8 > = cstr8!(   "xaudio2_9.dll");
+#[doc = "`\"xaudio2_9.dll\"`"] pub const XAUDIO2_DLL_W      : CStrNonNull<'static, u16> = cstr16!(  "xaudio2_9.dll");
+
+#[doc = "`\"xaudio2_9d.dll\"`"] pub const XAUDIO2D_DLL      : &'static str              =           "xaudio2_9d.dll";
+#[doc = "`\"xaudio2_9d.dll\"`"] pub const XAUDIO2D_DLL_A    : CStrNonNull<'static, u8 > = cstr8!(   "xaudio2_9d.dll");
+#[doc = "`\"xaudio2_9d.dll\"`"] pub const XAUDIO2D_DLL_W    : CStrNonNull<'static, u16> = cstr16!(  "xaudio2_9d.dll");
+
+#[doc = "`2B02E3CF-2E0B-4ec3-BE45-1B2A3FE7210D`"] pub const IID_IXAudio2          : GUID = super::guid(0x2B02E3CF, 0x2E0B, 0x4ec3, 0xBE451B2A3FE7210D);
+#[doc = "`84ac29bb-d619-44d2-b197-e4acf7df3ed6`"] pub const IID_IXAudio2Extension : GUID = super::guid(0x84ac29bb, 0xd619, 0x44d2, 0xb197e4acf7df3ed6);
