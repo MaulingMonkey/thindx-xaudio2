f526c65dcf73b003d061b9b00fa012c5aea9d7ad
MaulingMonkey <git@maulingmonkey.com>
2022-10-02 10:19:06 -0700

    "XAudio2 constants, flags and error codes." batch

--- crates/thindx-xaudio2-sys/src/sys28.rs
+++ crates/thindx-xaudio2-sys/src/sys28.rs
@@ -9,6 +9,7 @@
 // don't use prev
 use abistr::*;
 use winapi::shared::guiddef::GUID;
+use winresult::*;
 
 #[doc = "`\"xaudio2_8.dll\"`"] pub const XAUDIO2_DLL    : &'static str              =           "xaudio2_8.dll";
 #[doc = "`\"xaudio2_8.dll\"`"] pub const XAUDIO2_DLL_A  : CStrNonNull<'static, u8 > = cstr8!(   "xaudio2_8.dll");
@@ -23,3 +24,146 @@ use winapi::shared::guiddef::GUID;
 #[doc(hidden)] pub const XAUDIO2D_DLL_W : CStrNonNull<'static, u16> = cstr16!(  "xaudio2_8.dll");
 
 #[doc = "`60d8dac8-5aa1-4e8e-b597-2f5e2883d484`"] pub const IID_IXAudio2 : GUID = super::guid(0x60d8dac8, 0x5aa1, 0x4e8e, 0xb5972f5e2883d484);
+
+
+
+// Numeric boundary values
+
+/// Maximum bytes allowed in a source buffer
+pub const XAUDIO2_MAX_BUFFER_BYTES : u32 = 0x80000000;
+
+/// Maximum buffers allowed in a voice queue
+pub const XAUDIO2_MAX_QUEUED_BUFFERS : u32 = 64;
+
+/// Maximum buffers allowed for system threads (Xbox 360 only)
+pub const XAUDIO2_MAX_BUFFERS_SYSTEM : u32 = 2;
+
+/// Maximum channels in an audio stream
+pub const XAUDIO2_MAX_AUDIO_CHANNELS : u32 = 64;
+
+/// Minimum audio sample rate supported
+pub const XAUDIO2_MIN_SAMPLE_RATE : u32 = 1000;
+
+/// Maximum audio sample rate supported
+pub const XAUDIO2_MAX_SAMPLE_RATE : u32 = 200000;
+
+/// Maximum acceptable volume level (2^24)
+pub const XAUDIO2_MAX_VOLUME_LEVEL : f32 = 16777216.0;
+
+/// Minimum SetFrequencyRatio argument
+pub const XAUDIO2_MIN_FREQ_RATIO : f32 = 1.0 / 1024.0;
+
+/// Maximum MaxFrequencyRatio argument
+pub const XAUDIO2_MAX_FREQ_RATIO : f32 = 1024.0;
+
+/// Default MaxFrequencyRatio argument
+pub const XAUDIO2_DEFAULT_FREQ_RATIO : f32 = 2.0;
+
+/// Maximum XAUDIO2_FILTER_PARAMETERS.OneOverQ
+pub const XAUDIO2_MAX_FILTER_ONEOVERQ : f32 = 1.5;
+
+/// Maximum XAUDIO2_FILTER_PARAMETERS.Frequency
+pub const XAUDIO2_MAX_FILTER_FREQUENCY : f32 = 1.0;
+
+/// Maximum non-infinite XAUDIO2_BUFFER.LoopCount
+pub const XAUDIO2_MAX_LOOP_COUNT : u32 = 254;
+
+/// Maximum simultaneous XAudio2 objects on Xbox 360
+pub const XAUDIO2_MAX_INSTANCES : u32 = 8;
+
+
+
+// For XMA voices on Xbox 360 there is an additional restriction on the MaxFrequencyRatio
+// argument and the voice's sample rate: the product of these numbers cannot exceed 600000
+// for one-channel voices or 300000 for voices with more than one channel.
+
+pub const XAUDIO2_MAX_RATIO_TIMES_RATE_XMA_MONO         : u32 = 600000;
+pub const XAUDIO2_MAX_RATIO_TIMES_RATE_XMA_MULTICHANNEL : u32 = 300000;
+
+
+
+// Numeric values with special meanings
+
+/// Used as an OperationSet argument
+pub const XAUDIO2_COMMIT_NOW : u32 = 0;
+
+/// Used in IXAudio2::CommitChanges
+pub const XAUDIO2_COMMIT_ALL : u32 = 0;
+
+/// Not allowed for OperationSet arguments
+pub const XAUDIO2_INVALID_OPSET : u32 = -1_i32 as u32;
+
+/// Used in XAUDIO2_BUFFER.LoopCount
+pub const XAUDIO2_NO_LOOP_REGION : u32 = 0;
+
+/// Used in XAUDIO2_BUFFER.LoopCount
+pub const XAUDIO2_LOOP_INFINITE : u32 = 255;
+
+/// Used in CreateMasteringVoice
+pub const XAUDIO2_DEFAULT_CHANNELS : u32 = 0;
+
+/// Used in CreateMasteringVoice
+pub const XAUDIO2_DEFAULT_SAMPLERATE : u32 = 0;
+
+
+
+// Flags
+
+/// Used in IXAudio2::CreateSourceVoice
+pub const XAUDIO2_VOICE_NOPITCH : u32 = 0x0002;
+
+/// Used in IXAudio2::CreateSourceVoice
+pub const XAUDIO2_VOICE_NOSRC : u32 = 0x0004;
+
+/// Used in IXAudio2::CreateSource/SubmixVoice
+pub const XAUDIO2_VOICE_USEFILTER : u32 = 0x0008;
+
+/// Used in IXAudio2SourceVoice::Stop
+pub const XAUDIO2_PLAY_TAILS : u32 = 0x0020;
+
+/// Used in XAUDIO2_BUFFER.Flags
+pub const XAUDIO2_END_OF_STREAM : u32 = 0x0040;
+
+/// Used in XAUDIO2_SEND_DESCRIPTOR.Flags
+pub const XAUDIO2_SEND_USEFILTER : u32 = 0x0080;
+
+/// Used in IXAudio2SourceVoice::GetState
+pub const XAUDIO2_VOICE_NOSAMPLESPLAYED : u32 = 0x0100;
+
+
+
+// Default parameters for the built-in filter
+
+//pub const XAUDIO2_DEFAULT_FILTER_TYPE       : ()  = LowPassFilter; // TODO
+pub const XAUDIO2_DEFAULT_FILTER_FREQUENCY  : f32 = XAUDIO2_MAX_FILTER_FREQUENCY;
+pub const XAUDIO2_DEFAULT_FILTER_ONEOVERQ   : f32 = 1.0;
+
+
+
+// Internal XAudio2 constants
+
+/// [XAUDIO2_QUANTUM_MS] numerator
+pub const XAUDIO2_QUANTUM_NUMERATOR : u32 = 1;
+
+/// [XAUDIO2_QUANTUM_MS] denominator
+pub const XAUDIO2_QUANTUM_DENOMINATOR : u32 = 100;
+
+/// On Windows, XAudio2 processes audio in `10`ms chunks (= 1/100 seconds)
+pub const XAUDIO2_QUANTUM_MS : f32 = 1000.0 * (XAUDIO2_QUANTUM_NUMERATOR as f32) / (XAUDIO2_QUANTUM_DENOMINATOR as f32);
+
+
+
+/// XAudio2 error codes
+pub const FACILITY_XAUDIO2 : HResultFacilityMicrosoft = HResultFacilityMicrosoft::from_constant(0x896);
+
+/// An API call or one of its arguments was illegal
+pub const XAUDIO2_E_INVALID_CALL : HResult = HResult::from_constant(0x88960001);
+
+/// The XMA hardware suffered an unrecoverable error
+pub const XAUDIO2_E_XMA_DECODER_ERROR : HResult = HResult::from_constant(0x88960002);
+
+/// XAudio2 failed to initialize an XAPO effect
+pub const XAUDIO2_E_XAPO_CREATION_FAILED : HResult = HResult::from_constant(0x88960003);
+
+/// An audio device became unusable (unplugged, etc)
+pub const XAUDIO2_E_DEVICE_INVALIDATED : HResult = HResult::from_constant(0x88960004);
--- crates/thindx-xaudio2-sys/src/sys29.rs
+++ crates/thindx-xaudio2-sys/src/sys29.rs
@@ -6,7 +6,7 @@
 //! *   [XAudio2 Versions: XAudio 2.9 (Windows 10 and redistributable for Windows 7 and Windows 8.x)](https://learn.microsoft.com/en-us/windows/win32/xaudio2/xaudio2-versions#xaudio-29-windows-10-and-redistributable-for-windows-7-and-windows-8x)
 //! *   [Developer guide for redistributable version of XAudio 2.9](https://learn.microsoft.com/en-us/windows/win32/xaudio2/xaudio2-redistributable)
 
-//use super::xaudio2_8 as prev;
+use super::xaudio2_8 as prev;
 use abistr::*;
 use winapi::shared::guiddef::GUID;
 
@@ -20,3 +20,87 @@ use winapi::shared::guiddef::GUID;
 
 #[doc = "`2B02E3CF-2E0B-4ec3-BE45-1B2A3FE7210D`"] pub const IID_IXAudio2          : GUID = super::guid(0x2B02E3CF, 0x2E0B, 0x4ec3, 0xBE451B2A3FE7210D);
 #[doc = "`84ac29bb-d619-44d2-b197-e4acf7df3ed6`"] pub const IID_IXAudio2Extension : GUID = super::guid(0x84ac29bb, 0xd619, 0x44d2, 0xb197e4acf7df3ed6);
+
+// Numeric boundary values
+pub use prev::{
+    XAUDIO2_MAX_BUFFER_BYTES,
+    XAUDIO2_MAX_QUEUED_BUFFERS,
+    XAUDIO2_MAX_BUFFERS_SYSTEM,
+    XAUDIO2_MAX_AUDIO_CHANNELS,
+    XAUDIO2_MIN_SAMPLE_RATE,
+    XAUDIO2_MAX_SAMPLE_RATE,
+    XAUDIO2_MAX_VOLUME_LEVEL,
+    XAUDIO2_MIN_FREQ_RATIO,
+    XAUDIO2_MAX_FREQ_RATIO,
+    XAUDIO2_DEFAULT_FREQ_RATIO,
+    XAUDIO2_MAX_FILTER_ONEOVERQ,
+    XAUDIO2_MAX_FILTER_FREQUENCY,
+    XAUDIO2_MAX_LOOP_COUNT,
+    XAUDIO2_MAX_INSTANCES,
+};
+
+pub use prev::{
+    XAUDIO2_MAX_RATIO_TIMES_RATE_XMA_MONO,
+    XAUDIO2_MAX_RATIO_TIMES_RATE_XMA_MULTICHANNEL,
+};
+
+// Numeric values with special meanings
+pub use prev::{
+    XAUDIO2_COMMIT_NOW,
+    XAUDIO2_COMMIT_ALL,
+    XAUDIO2_INVALID_OPSET,
+    XAUDIO2_NO_LOOP_REGION,
+    XAUDIO2_LOOP_INFINITE,
+    XAUDIO2_DEFAULT_CHANNELS,
+    XAUDIO2_DEFAULT_SAMPLERATE,
+};
+
+
+
+// Flags
+pub use prev::{
+    XAUDIO2_VOICE_NOPITCH,
+    XAUDIO2_VOICE_NOSRC,
+    XAUDIO2_VOICE_USEFILTER,
+    XAUDIO2_PLAY_TAILS,
+    XAUDIO2_END_OF_STREAM,
+    XAUDIO2_SEND_USEFILTER,
+    XAUDIO2_VOICE_NOSAMPLESPLAYED,
+};
+
+/// Used in XAudio2Create
+pub const XAUDIO2_DEBUG_ENGINE : u32 = 0x0001;
+
+/// Used in XAudio2Create to force the engine to Stop when no source voices are Started, and Start when a voice is Started
+pub const XAUDIO2_STOP_ENGINE_WHEN_IDLE : u32 = 0x2000;
+
+/// Used in XAudio2Create to specify nondefault processing quantum of 21.33 ms (1024 samples at 48KHz)
+pub const XAUDIO2_1024_QUANTUM : u32 = 0x8000;
+
+/// Used in CreateMasteringVoice to create a virtual audio client
+pub const XAUDIO2_NO_VIRTUAL_AUDIO_CLIENT : u32 = 0x10000;
+
+
+
+// Default parameters for the built-in filter
+pub use prev::{
+    //XAUDIO2_DEFAULT_FILTER_TYPE, // TODO
+    XAUDIO2_DEFAULT_FILTER_FREQUENCY,
+    XAUDIO2_DEFAULT_FILTER_ONEOVERQ,
+};
+
+// Internal XAudio2 constants
+pub use prev::{
+    XAUDIO2_QUANTUM_NUMERATOR,
+    XAUDIO2_QUANTUM_DENOMINATOR,
+    XAUDIO2_QUANTUM_MS,
+};
+
+// XAudio2 error codes
+pub use prev::{
+    FACILITY_XAUDIO2,
+    XAUDIO2_E_INVALID_CALL,
+    XAUDIO2_E_XMA_DECODER_ERROR,
+    XAUDIO2_E_XAPO_CREATION_FAILED,
+    XAUDIO2_E_DEVICE_INVALIDATED,
+};
