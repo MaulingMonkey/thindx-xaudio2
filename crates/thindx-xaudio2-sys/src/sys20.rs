54335973adc1d4a5944fed9b6241f905b5d6b791
MaulingMonkey <git@maulingmonkey.com>
2022-10-02 09:41:06 -0700

    XAudio ..= 2.7: warn about IID_IXAudio2 reuse

--- crates/thindx-xaudio2-sys/src/sys20.rs
+++ crates/thindx-xaudio2-sys/src/sys20.rs
@@ -10,4 +10,9 @@ use winapi::shared::guiddef::GUID;
 
 pub const CLSID_XAudio2         : GUID = super::guid(0xfac23f48, 0x31f5, 0x45a8, 0xb49b5225d61401aa);
 pub const CLSID_XAudio2_Debug   : GUID = super::guid(0xfac23f48, 0x31f5, 0x45a8, 0xb49b5225d61401db);
+
+/// ⚠️ **WARNING:** This IID is reused between XAudio 2.0 through 2.7, despite different interface shapes,
+/// in violation of [the COM interface contract rules](https://devblogs.microsoft.com/oldnewthing/20051101-54/?p=33533).
+/// This may cause undefined behavior if you mix multiple versions of XAudio2 within the same process.
+/// ⚠️
 pub const IID_IXAudio2          : GUID = super::guid(0x8bcf1f58, 0x9fe7, 0x4583, 0x8ac6e2adc465c8bb);
