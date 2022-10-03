f8b32ba436f2f3ac92df553d92de1d3e6b1c642d
MaulingMonkey <git@maulingmonkey.com>
2022-10-03 13:30:23 -0700

    xaudio2_8: match structure of xaudio2_9

--- crates/thindx-xaudio2/src/xa28.rs
+++ crates/thindx-xaudio2/src/xa28.rs
@@ -6,14 +6,27 @@
 //! *   [XAudio2 Versions: XAudio 2.8 (Windows 8.x)](https://learn.microsoft.com/en-us/windows/win32/xaudio2/xaudio2-versions#xaudio-28-windows-8x)
 //! *   [XAudio2 and Windows 8](https://walbourn.github.io/xaudio2-and-windows-8/)
 
+
+
+// Might not remain pub
+#[doc(hidden)] pub use xaudio2_sys::{
+    IXAudio2,
+
+};
+
 /// Raw low level FFI bindings
 ///
-pub use thindx_xaudio2_sys::xaudio2_8 as sys;
+pub use thindx_xaudio2_sys::xaudio2_8 as xaudio2_sys;
 
-pub use sys::XAUDIO2_DLL    as DLL;
-pub use sys::XAUDIO2_DLL_A  as DLL_A;
-pub use sys::XAUDIO2_DLL_W  as DLL_W;
+/// `XAudio2*` & `XAUDIO2_*`
+pub mod xaudio2 {
+    pub use super::xaudio2_sys as sys;
 
-pub use sys::XAUDIO2D_DLL    as D_DLL;
-pub use sys::XAUDIO2D_DLL_A  as D_DLL_A;
-pub use sys::XAUDIO2D_DLL_W  as D_DLL_W;
+    pub use sys::XAUDIO2_DLL    as DLL;
+    pub use sys::XAUDIO2_DLL_A  as DLL_A;
+    pub use sys::XAUDIO2_DLL_W  as DLL_W;
+
+    pub use sys::XAUDIO2D_DLL    as D_DLL;
+    pub use sys::XAUDIO2D_DLL_A  as D_DLL_A;
+    pub use sys::XAUDIO2D_DLL_W  as D_DLL_W;
+}
