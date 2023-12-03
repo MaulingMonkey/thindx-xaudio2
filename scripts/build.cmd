0ff8b685e0c704888d1b9ce5c4a28cfe3bae3011
MaulingMonkey <git@maulingmonkey.com>
2023-12-03 15:38:39 -0800

    scripts\build.cmd: -Z doctest-in-workspace stabilized

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,12 +1,7 @@
 @pushd "%~dp0.." && setlocal
 
 :: all features
-:: https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#doctest-in-workspace : fixes paths for problems tab
-cargo +nightly test --all-features -Z doctest-in-workspace
-@if ERRORLEVEL 1 goto :die
-
-:: no features, no doctest-in-workspace
-cargo test
+cargo test --all-features
 @if ERRORLEVEL 1 goto :die
 
 :die
