8249a7b8733d688d8649d1017d6ebab744c0fa67
MaulingMonkey <git@maulingmonkey.com>
2024-02-17 14:49:08 -0800

    docs: add "Thread Safety" to `thindx-xaudio2 as well

--- crates/thindx-xaudio2/doc/_doc.rs
+++ crates/thindx-xaudio2/doc/_doc.rs
@@ -4,6 +4,7 @@
 //! [Crate Features](features),
 // //! [Design Decisions](design),
 // //! [Environment Variables](environment),
+//! [Thread Safety](threading),
 //! [XAudio2 Versions](versions)
 
 pub use crate::sys::_doc::*;
